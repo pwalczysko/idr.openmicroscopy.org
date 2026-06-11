f862034d6886584117a3bc515571a0676258dcfd
pwalczysko <p.walczysko@dundee.ac.uk>
2026-06-11 15:06:03 +0100

    Try lychee

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 set -u
 
 docker run --rm -v $PWD:/srv/jekyll -eJEKYLL_UID=$UID jekyll/builder:pages jekyll build --config _config.yml,_prod.yml
-docker run --rm -v $PWD/_site:/site/about jekyll/builder:pages /usr/gem/bin/htmlproofer /site --ignore-urls "/jupyter/,/filezilla-project.org/,/webclient/,/cell/,/tissue/,/login.binder.bioimagearchive.org/,/biii.eu/,/ncbi.nlm.nih.gov/,/localhost/,/github.com/" --only_4xx --no-enforce-https --allow-missing-href --ignore-status-codes "400,404" --typhoeus='{
+docker run --rm -v $PWD/_site:/site/about jekyll/builder:pages /usr/gem/bin/htmlproofer /site  --disable-external --ignore-urls "/jupyter/,/filezilla-project.org/,/webclient/,/cell/,/tissue/,/login.binder.bioimagearchive.org/,/biii.eu/,/ncbi.nlm.nih.gov/,/localhost/,/github.com/" --only_4xx --no-enforce-https --allow-missing-href --ignore-status-codes "400,404" --typhoeus='{
   "headers":{
     "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36"
   },
@@ -13,3 +13,5 @@ docker run --rm -v $PWD/_site:/site/about jekyll/builder:pages /usr/gem/bin/html
   "connecttimeout":10,
   "accept_encoding":"gzip"
 }'
+
+lychee --method get _site
